54c7e4e6a66b4e37403bfd17f1fab813aff8fbd9
WelshDan <dan.roberts78@gmail.com>
2023-03-21 19:30:25 +0000

    Type out list of possible functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,41 @@
+const gameGrid = document.querySelector("#game-grid")
+
+
 let x = <i class="fa-solid fa-x" style="color: blue;">
 let o = <i class="fa-solid fa-o" style="color: red;">
 
 const win =[
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
     [1,4,7],
     [2,5,8],
-    [3,6,9],
-    [1,5,9],
-    [3,5,7],
-]
+    [0,4,8],
+    [2,4,6],
+];
+
+
+function runGame(); {
+    
+}
+
+function checkMove(); {
+
+}
+
+function insertSign() {
+
+}
+
+function nextPlayer() {
+
+}
+
+function calculateWinner() {
+
+}
+
+function newGame() {
+
+}
