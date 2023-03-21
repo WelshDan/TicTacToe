6fbc906404f331fe23595ef502d8f9a52ff0f985
WelshDan <dan.roberts78@gmail.com>
2023-03-21 23:53:26 +0000

    Add function to create X or O to game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,29 @@
-const square = document.querySelectorAll('[square]')
+let isPlayer2 = false;
+let newGame = false;
+const string = newGame ? "" : (isPlayer2 ? "O" : "X");
+ function add() {
+    isPlayer2 = true;
+    document.getElementById("00").innerHTML = string;
+    newGame = false;
+    alert(string);
+  }
+
+
+
+
+
+  let square = document.getElementById('0');
+  square.addEventListener('click', add);
+
+
+
+
+
+
 const gameGrid = document.getElementById(".game-grid")
 const statusDisplay = document.querySelector(".status")
-const newGame = document.querySelector(".playAgain")
 
-const player1 = <i class="fa-solid fa-x" style="color: blue;" />
-const player2 = <i class="fa-solid fa-o" style="color: red;" />
-let isPlayer2 = false;
+
 
 const won =[
     [0,1,2],
@@ -17,21 +35,23 @@ const won =[
     [0,4,8],
     [2,4,6],
 ];
-
+/*
 newGame()
 playAgain.addEventListener('click', newGame)
 function newGame() {
     isPlayer2 = false
         square.classList.remove();
 }
-const square0 = document.getElementById("0");
-square0.addEventListener('click', clickSquare);
+
+const element = document.getElementById("0");
+element.addEventListener('click', clickSquare);
 function clickSquare() {
-    console.log("Hello");
+    alert("Hello");
 }
 
-function playerControl() {
-    if (player1) 
+function playerControl() 
+    if (player1) {
+
 }
 
 function checkMove(); {
@@ -51,3 +71,4 @@ function nextPlayer() {
 function calculateWinner() {
 
 }
+*/
