13bdafd6b1411e3b359b17512c7b7a6283399105
WelshDan <dan.roberts78@gmail.com>
2023-03-22 09:48:19 +0000

    Add more rules to the js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
-})
+const box = document.querySelectorAll("square");
+const statusDisplay = document.querySelector(".status");
+const restartGame = document.getElementById("restart");
+const winCheck =[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
 
-let startingPlayer = player1;
-let enterImg = (player1 ? "x" : "o");
-document.addEventListener("click", enterImg)
-function enterImg() {
-}
+let options = ["", "", "", "", "", "", "", "", "",]
 
 
 /*
@@ -20,23 +27,14 @@ const string = newGame ? "" : (isPlayer2 ? "O" : "X");
   }
 /*
 
-const startGrid = [ "", "", "", "", "", "", "", "", "",]
+
   let square = document.getElementById('0');
   square.addEventListener('click', add);
 
-const gameGrid = document.getElementById(".game-grid")
-const statusDisplay = document.querySelector(".status")
 
-const winCheck =[
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],
-];
+
+
+
 
 function startGame () {
 
